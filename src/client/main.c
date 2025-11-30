8c9e656479fc17a0e65fe8cd62df74e32d572455
root <root@jonslapot.>
2025-11-29 20:01:01 -0500

    what i have from friday

--- src/client/main.c
+++ src/client/main.c
@@ -4,6 +4,7 @@ int main() {
     int sock = create_socket();
     struct sockaddr_in server_addr;
     char buffer[BUF_SIZE] = {0};
+    char message[BUF_SIZE];
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
@@ -17,8 +18,26 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    read(sock, buffer, BUF_SIZE);
-    printf("Received: %s", buffer);
+    printf("Connected to server. Type messages (or 'quit' to exit):\n");
+
+    while (1) {
+        printf("> ");
+        fgets(message, BUF_SIZE, stdin);
+        
+        if (strncmp(message, "quit", 4) == 0) {
+            break;
+        }
+        
+        send(sock, message, strlen(message), 0);
+        
+        int bytes = read(sock, buffer, BUF_SIZE-1);
+        if (bytes > 0) {
+            buffer[bytes] = '\0';
+            printf("Server: %s", buffer);
+        }
+        
+        memset(buffer, 0, BUF_SIZE);
+    }
 
     close(sock);
     return 0;
